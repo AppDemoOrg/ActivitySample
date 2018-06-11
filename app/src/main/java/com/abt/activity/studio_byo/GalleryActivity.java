9e32c3ced5462e39fb5cbc87f4416a6ac6e5e6c0
huangweiqi <askviky2010@gmail.com>
2018-06-12 00:33:58 +0800

    BottomNavigationActivity，666

---
+++ app/src/main/java/com/abt/activity/studio_byo/GalleryActivity.java
@@ -0,0 +1,15 @@
+package com.abt.activity.studio_byo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.abt.activity.R;
+
+public class GalleryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+    }
+}
